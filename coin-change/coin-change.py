4789693048e0532173907f2c4f22cfe81c48cd62
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-08 20:59:10 +0300

    Time: 1984 ms (20.35%), Space: 14.4 MB (92.80%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -6,9 +6,11 @@ class Solution:
         dp = [amount + 1] * (amount + 1)
         dp[0] = 0
         
-        for am in range(amount + 1):
+        for am in range(1, amount + 1):
             for coin in coins:
+                # пропускаем монету если она больше чем сумма am
                 if am - coin >= 0:
+                    # если меньше - пробуем ее использовать с комбинацей для разности am - coin
                     dp[am] = min(dp[am], 1 + dp[am - coin])
                     
         return dp[amount] if dp[amount] != amount + 1 else -1
