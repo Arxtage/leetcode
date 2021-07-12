81e5fb53a440758951514f5fc36aab742c46ccd7
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-12 21:36:16 +0300

    Time: 140 ms (19.77%), Space: 17.6 MB (57.59%) - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,27 +1,6 @@
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         # o()
-#         d = {} # dict:list of anagrams
-        
-#         for str in strs:
-#             dict1 = {}
-#             for ltr in str:
-#                 if ltr in dict1.keys():
-#                     dict1[ltr]+=1
-#                 else:
-#                     dict1[ltr] = 1
-#             if dict1 in d.keys():
-#                 d[dict1].append(str)
-#             else:
-#                 d[dict1] = [str]
-            
-            
-#         for key in d.keys():
-#             print(d[key])
-    
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # using sort
+        # using sort o(n*logn*m) where n - avg len of word, m - num of strings
         d = {}
         for str in strs:
             tmp = ''.join(sorted(str))
