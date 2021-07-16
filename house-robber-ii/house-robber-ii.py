8ad512746166bede79d4602984185d404732b2e6
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-16 17:49:13 +0300

    Time: 28 ms (88.30%), Space: 14.2 MB (53.91%) - LeetHub

--- house-robber-ii/house-robber-ii.py
+++ house-robber-ii/house-robber-ii.py
@@ -2,12 +2,9 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         # DP. House Robber I twice
         
-        if len(nums) == 1:
-            return(nums[0])
-        def houseRobber(nums):
+        def houseRobber(nums, DP):
             if len(nums) == 1:
                 return(nums[0])
-            DP = [0]*len(nums)
             DP[0] = nums[0]
             DP[1] = max(nums[0:2])
             for i in range(2,len(nums)):
@@ -16,5 +13,9 @@ class Solution:
 
             return(DP[-1])
         
-        return(max(houseRobber(nums[:-1]), houseRobber(nums[1:])))
+        if len(nums) == 1:
+            return(nums[0])
+        
+        DP = [0]*(len(nums)-1)
+        return(max(houseRobber(nums[:-1], DP), houseRobber(nums[1:], DP)))
         
