5e38623cfca09108d61cf5d964687ae1014f98f5
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-17 23:34:10 +0300

    Time: 980 ms (56.21%), Space: 17.4 MB (86.15%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,35 +1,17 @@
 class Solution:
     # o(n^2)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    
-        res = []
-#         def twoSum(numbers: List[int], target: int) -> List[int]:
-#             L = 0
-#             R = len(numbers) - 1
-
-#             while L < R:
-#                 if numbers[L] + numbers[R] - target > 0:
-#                     R-=1
-#                 elif numbers[L] + numbers[R] - target < 0:
-#                     L+=1
-#                 elif numbers[L] + numbers[R] - target == 0:
-#                     res.append([numbers[L], numbers[R], -target])
-#                     L+=1
-#                     while nums[L] == nums[L-1]:
-#                         L+=1
                 
-        
+        res = []
         nums.sort()
 
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-                
-            # twoSum(numbers = nums[:i] + nums[i+1:], target = - nums[i])
-            
+                            
+            # TwoSum II
             L = i+1
             R = len(nums) - 1
-
             while L < R:
                 if nums[L] + nums[R] + nums[i] > 0:
                     R-=1
