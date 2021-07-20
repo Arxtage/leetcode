80c6d095ccfe2e5adb2d004d529a32f8115183ce
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-20 03:10:53 +0300

    Time: 28 ms (90.57%), Space: 14.1 MB (88.23%) - LeetHub

--- reverse-bits/reverse-bits.py
+++ reverse-bits/reverse-bits.py
@@ -1,19 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = ''
-        # n to bits and reverse
-        for i in range(32,-1,-1):
-
-            if n - 2**i >=0:
-                res = "1" + res
-                n = n - 2**i
-            else:
-                res = "0" + res
-
-        
-        # convert back bits
-        fin = 0
-        for i in range(len(res)):
-            if int(res[i]):
-                fin +=2**(len(res) - int(i)-2)
-        return(fin)
+        # bit operations o(1)
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
+        return(res)
