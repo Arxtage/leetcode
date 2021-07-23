70ca180640709de81414f92d49ee7e48353283fe
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-24 01:31:11 +0300

    Time: 104 ms (51.85%), Space: 17.4 MB (15.33%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,27 +1,28 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # O(E + V)
+#       O(E + V)
         hmap = {i:[] for i in range(n)}
         for edge in edges:
             hmap[edge[0]].append(edge[1])
             hmap[edge[1]].append(edge[0])
             
-        visited = []
+        visited = set()
         def dfs(node):
             # base returns:
-            if node in visited or len(visited) == n:
+            if node in visited:
                 return
-            visited.append(node)
+            visited.add(node)
             if hmap[node] != []:
                 for nei in hmap[node]:
                     dfs(nei)
 
         count = 0
         for node in range(n):
+            if len(visited) == n: 
+                break
             if node not in visited:
                 dfs(node)
                 count += 1
-            if len(visited) == n:
-                return(count)
+        return(count)
         
         
