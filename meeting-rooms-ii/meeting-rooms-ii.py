44a440ac1450d0acc75752b6381a8fc23393a325
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-25 00:37:31 +0300

    Time: 68 ms (97.01%), Space: 17.5 MB (24.54%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.py
+++ meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,17 +1,32 @@
 class Solution:
+#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+#         # O(N^2) worth case when all lectures are at the same time. O(N*Output) 
+        
+#         intervals.sort()
+#         maxi = 1
+#         rooms = [intervals[0][1]]
+        
+#         for interv in intervals[1:]:
+#             for i in range(len(rooms)):
+#                 if rooms[i] <= interv[0]:
+#                     rooms[i] = interv[1]
+#                     break
+#                 if i == (len(rooms) - 1):  
+#                     rooms.append(interv[1])
+#             maxi = max(maxi, len(rooms))
+#         return(maxi)
+    
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # O(N^2) worth case when all lectures are at the same time. O(N*Output) 
+        # O(NlogN)
         
         intervals.sort()
-        maxi = 1
-        rooms = [intervals[0][1]]
+        rooms = []
         
+        heapq.heappush(rooms, intervals[0][1])
+
         for interv in intervals[1:]:
-            for i in range(len(rooms)):
-                if rooms[i] <= interv[0]:
-                    rooms[i] = interv[1]
-                    break
-                if i == (len(rooms) - 1):  
-                    rooms.append(interv[1])
-            maxi = max(maxi, len(rooms))
-        return(maxi)
+            if rooms[0] <= interv[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, interv[1])
+            
+        return len(rooms)
