af78d9553f224d6cd5f1e43a4f8b050f24784bed
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-28 00:42:01 +0300

    Time: 28 ms (79.94%), Space: 14.1 MB (72.89%) - LeetHub

--- guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -6,19 +6,16 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
         # Binary Search O(logn)
-        if n == 1:
-            return(1)
         
         L, R = 0, n
         res = 1
         while L <= R:
             mid = (L+R) // 2
             res = guess(mid)
-            if res ==0:
+            if res == 0:
                 return(mid)
             if res  == -1:
                 R = mid - 1
             if res == 1:
                 L = mid + 1
                 
-        # return(L)
