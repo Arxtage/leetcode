5d6fa2207127848f2c09a1484acc8282fc8ad0f3
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-28 23:05:58 +0300

    Time: 366 ms (10.42%), Space: 16.3 MB (12.31%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,48 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        # Find Pivot in nums with Binary Search
-        # Merge sort
-        # return squares
+        # O(N)
+        res = [0]*len(nums)
         
-        # find pivot:
+        L, R = 0, len(nums) - 1
         
-        length = len(nums)
-        
-        if len(nums) == 1:
-            positive_ind = 0
-        else:
-            positive_ind = -1
-        
-        for i in range(length):
-            if nums[i] >= 0:
-                positive_ind = i
-                break
-            
-        
-        nums_neg = nums[:positive_ind][::-1]
-        nums_pos = nums[positive_ind:]
-        
-        # merge sort and square:
-        i = j = k =  0
-        while i < len(nums_neg) and j < len(nums_pos):
-            if (-nums_neg[i]) < nums_pos[j]:
-                nums[k] = nums_neg[i]**2
-                i += 1
+        for i in range(len(nums) - 1, -1, -1):
+            if abs(nums[L]) <= abs(nums[R]):
+                val = nums[R]
+                R -= 1
             else:
-                nums[k] = nums_pos[j]**2
-                j += 1
-            k += 1
-        
-        # Checking if any element was left
-        while i < len(nums_neg):
-            nums[k] = nums_neg[i]**2
-            i += 1
-            k += 1
-            
-        while j < len(nums_pos):
-            nums[k] = nums_pos[j]**2
-            j += 1
-            k += 1
-        
-        return(nums)
+                val = nums[L]
+                L += 1
+            res[i] = val * val
+        return(res)
             
