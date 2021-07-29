63a7577335c6eefc1bc9e85c11e4cc66a35eff54
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-07-29 21:32:09 +0300

    Time: 880 ms (68.59%), Space: 17.6 MB (51.95%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,25 +1,30 @@
 class Solution:
-    # o(n^2)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-                
-        res = []
         nums.sort()
-
+        
+        res = []
+        
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-                            
-            # TwoSum II
-            L = i+1
-            R = len(nums) - 1
+                
+        
+           # O(n) two pointers of Two Nums II
+            L, R = i + 1, len(nums) - 1
+
             while L < R:
-                if nums[L] + nums[R] + nums[i] > 0:
-                    R-=1
-                elif nums[L] + nums[R] + nums[i] < 0:
-                    L+=1
-                elif nums[L] + nums[R] + nums[i] == 0:
+
+                triplet_sum = nums[i] + nums[L] + nums[R]
+                if triplet_sum < 0:
+                    L += 1
+                elif triplet_sum > 0:
+                    R -=1
+                else:
                     res.append([nums[i], nums[L], nums[R]])
                     L+=1
+                    R-=1
                     while nums[L] == nums[L-1] and L < R:
                         L+=1
         return(res)
+    
+    
