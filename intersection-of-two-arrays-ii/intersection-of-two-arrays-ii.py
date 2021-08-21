cc3f51b6a62413f0e537c0d4290ea9752c4e868a
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-08-21 16:50:14 +0300

    Time: 70 ms (12.65%), Space: 14.4 MB (43.82%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,22 +1,38 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        # O(nlogn + mlogm)
-        nums1.sort()
-        nums2.sort()
+#         # O(nlogn + mlogm)
+#         nums1.sort()
+#         nums2.sort()
         
-        ind1 = 0
-        ind2 = 0
+#         ind1 = 0
+#         ind2 = 0
         
+#         res = []
+        
+#         while ind1 < len(nums1) and ind2 < len(nums2):
+#             if nums1[ind1] == nums2[ind2]:
+#                 res.append(nums1[ind1])
+#                 ind1 += 1
+#                 ind2 += 1
+#             elif nums1[ind1] < nums2[ind2]:
+#                 ind1 += 1
+#             else:
+#                 ind2 += 1
+#         return res 
+
+        # optimal O(n + m)
         res = []
+        hashmap = collections.defaultdict(int)
         
-        while ind1 < len(nums1) and ind2 < len(nums2):
-            if nums1[ind1] == nums2[ind2]:
-                res.append(nums1[ind1])
-                ind1 += 1
-                ind2 += 1
-            elif nums1[ind1] < nums2[ind2]:
-                ind1 += 1
-            else:
-                ind2 += 1
-        return res 
+        
+        for val in nums1:
+            hashmap[val] += 1
+        
+        for val in nums2:
+            if hashmap[val]:
+                res.append(val)
+                hashmap[val] -= 1
+                
+        return res
+                
                 
