12b7e28c846b040cc8a3ca9bb986242c892b6a56
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-08-22 18:05:00 +0300

    Time: 80 ms (50.52%), Space: 14.4 MB (34.34%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -1,15 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # O()
-        # Use two ounter dicts
-        s1_d = collections.Counter(s1)        
+        # Use two counter dicts
+        
         L, R = 0, len(s1) - 1
         
+        s1_d = collections.Counter(s1)
+        s2_d = collections.Counter(s2[L:R + 1])
+        
         while R < len(s2):
-
-            s2_d = collections.Counter(s2[L:R + 1])
+                
             if s1_d == s2_d:
                 return True
+            
             L += 1
             R += 1
+            
+            if R < len(s2):
+                s2_d[s2[R]] += 1
+            
+            if s2_d[s2[L - 1]] != 1:
+                s2_d[s2[L - 1]] -= 1
+            else:
+                del s2_d[s2[L - 1]]
+            
         return False
