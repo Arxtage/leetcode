4850d0538eae0a5962ef00d638d653ae1c363077
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-10-02 17:01:25 +0300

    Time: 37 ms (30.26%), Space: 14.4 MB (14.67%) - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -4,15 +4,13 @@ class Solution:
         
         L = 0
         R = 0
-        res = 0
         
         while R < len(nums):
             if nums[R] == val:
                 R += 1
             else:
                 nums[L] = nums[R]
-                res += 1
                 L += 1
                 R += 1
                 
-        return res
+        return L
