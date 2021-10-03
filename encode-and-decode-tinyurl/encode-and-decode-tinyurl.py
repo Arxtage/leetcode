785974d1eb145309d6abb9ff497a05b0c540acbc
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-10-03 19:01:47 +0300

    Time: 36 ms (54.90%), Space: 14.3 MB (29.39%) - LeetHub

--- encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,14 +1,21 @@
+import time
+
 class Codec:
-    # Dummy solution
+    # Using defaultfict
+    def __init__(self):
+        self.database = collections.defaultdict(str)
+        
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
-        """ 
-        return longUrl
+        """
+        shortUrl = "http://tinyurl.com/" + str(time.time())
+        self.database[shortUrl] = longUrl
+        return shortUrl
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return shortUrl
+        return self.database[shortUrl]
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
