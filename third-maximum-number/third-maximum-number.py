e94ff3ba8ec5a8f614278b38efd636cac8298089
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-10-16 20:47:21 +0300

    Time: 48 ms (91.05%), Space: 15.7 MB (9.18%) - LeetHub

--- third-maximum-number/third-maximum-number.py
+++ third-maximum-number/third-maximum-number.py
@@ -1,12 +1,29 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        # O(N)
-        nums = list(set(nums))
+        # O(N) with heap
+        nums = set(nums)
+        if len(nums) < 3:
+            return max(nums)
+        else:
+            return heapq.nlargest(3, nums)[2]
+    
+    
+    
+#     def thirdMax(self, nums: List[int]) -> int:
+#         # O(N)
         
-        maxim = nums.pop(nums.index(max(nums)))
-        if len(nums) < 2: # bcs already popped first max
-            return maxim
+#         maxim = nums.pop(nums.index(max(nums)))
+#         third_max = maxim
         
-        for iteration in range(2):
-            maxim = nums.pop(nums.index(max(nums)))
-        return maxim
+#         iterations = 0
+#         while iterations < 2 and len(nums) > 0:
+#             new = nums.pop(nums.index(max(nums)))
+            
+#             if maxim != new:
+#                 maxim = new
+#                 iterations += 1
+            
+#             else:
+#                 continue
+                
+#         return third_max if iterations == 1 else maxim
