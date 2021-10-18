d640a66f6b72f37dd0517a874c5db5d26047751d
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-10-18 23:33:55 +0300

    Time: 1067 ms (7.57%), Space: 30.1 MB (5.01%) - LeetHub

---
+++ island-perimeter/island-perimeter.py
@@ -0,0 +1,33 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        # DFS # O(m*n) worth case, but O(k) on average where k - num of land cells
+        
+        def dfs(row, col):
+            nonlocal perimeter
+            # base
+            if (row, col) in visited:
+                return
+            
+            if ROWS <= row or row < 0  or COLS <= col or col < 0 or grid[row][col] == 0:
+                perimeter += 1
+                return
+        
+            visited.add((row, col))
+            dfs(row + 1, col)
+            dfs(row - 1, col)
+            dfs(row, col + 1)
+            dfs(row, col - 1)
+            return
+        
+        
+        perimeter = 0
+        visited = set()
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        # find the first land
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+                    break
+        return perimeter
