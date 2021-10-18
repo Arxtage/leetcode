94dced54e9a76e1abfd8e2d4c6d5c73dc18e2a30
Arman Tsaturyan <31474005+Arxtage@users.noreply.github.com>
2021-10-18 21:53:46 +0300

    Time: 58 ms (6.98%), Space: 14.3 MB (54.87%) - LeetHub

--- pascals-triangle/pascals-triangle.py
+++ pascals-triangle/pascals-triangle.py
@@ -2,9 +2,11 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         # DP O(n^2)
         
-        triangle = [[1 for i in range(j)] for j in range(1, numRows + 1)]
-        
-        for j in range(2, numRows):
+        triangle = []
+        for j in range(numRows):
+            row = [1] * (j+1)
             for i in range(1, j):
-                triangle[j][i] = triangle[j - 1][i - 1] + triangle[j - 1][i]
+                row[i] = triangle[j - 1][i - 1] + triangle[j - 1][i]
+            triangle.append(row)
+                
         return triangle
